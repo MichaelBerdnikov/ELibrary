12805348c9e33bbed53e7b8168a0e67b5c9a5b4d
MichaelBerdnikov <mikeberd@mail.ru>
2016-12-15 22:14:45 +0300

    Add some test data

---
+++ insert_data.sql
@@ -0,0 +1,6 @@
+﻿
+INSERT INTO theme (theme_name) VALUES ('Детектив');
+INSERT INTO theme (theme_name) VALUES ('Драма');
+
+INSERT INTO book (book_isbn, book_name, book_path, theme_name, publisher_id) VALUES ('abc', 'abc', NULL, 'Детектив', NULL);
+INSERT INTO book (book_isbn, book_name, book_path, theme_name, publisher_id) VALUES ('abc2', 'abc2', NULL, 'Драма', NULL);
