c6b3d8408ee1bf8b1fd3c3d03ce5462de9c95d2f
MichaelBerdnikov <mikeberd@mail.ru>
2016-12-18 21:33:46 +0300

    update

--- create_tables.sql
+++ create_tables.sql
@@ -2,6 +2,14 @@
 DROP TABLE IF EXISTS test_table CASCADE;
 DROP TABLE IF EXISTS publisher CASCADE;
 DROP TABLE IF EXISTS theme CASCADE;
+DROP TABLE IF EXISTS book CASCADE;
+DROP TABLE IF EXISTS reader_book CASCADE;
+DROP TABLE IF EXISTS author_book CASCADE;
+DROP TABLE IF EXISTS person CASCADE;
+DROP TABLE IF EXISTS author CASCADE;
+DROP TABLE IF EXISTS reader CASCADE;
+DROP TABLE IF EXISTS commen CASCADE;
+
 
 CREATE TABLE test_table (
 	test_table_id BIGINT NOT NULL,
@@ -41,4 +49,59 @@ CREATE TABLE book (
 	CONSTRAINT book_pk PRIMARY KEY (book_ISBN),
 	CONSTRAINT theme_fk FOREIGN KEY (theme_name) REFERENCES theme (theme_name),
 	CONSTRAINT publisher_fk FOREIGN KEY (publisher_id) REFERENCES publisher (publisher_id)
+);
+
+CREATE TABLE reader_book (
+        reader_book_book_ISBN TEXT,
+        reader_book_reader_ID BIGINT,
+
+        CONSTRAINT reader_book_pk PRIMARY KEY (reader_book_book_ISBN, reader_book_reader_ID)        
+);
+
+CREATE TABLE author_book (
+        author_book_book_ISBN TEXT,
+        author_book_reader_ID BIGINT,
+
+        CONSTRAINT author_book_pk PRIMARY KEY (author_book_book_ISBN, author_book_reader_ID)        
+);
+
+CREATE TABLE person (
+        person_ID BIGINT,
+        person_last_name TEXT,
+        person_first_name TEXT,
+        person_middle_name TEXT,
+        person_birthdate DATE,
+
+        CONSTRAINT person_pk PRIMARY KEY (person_ID)
+
+);
+
+CREATE TABLE author (
+        author_ID BIGINT,
+                
+        CONSTRAINT author_pk PRIMARY KEY (author_ID),
+        CONSTRAINT person_fk FOREIGN KEY (author_ID) REFERENCES person (person_ID)
+);
+
+CREATE TABLE reader (
+        reader_ID BIGINT,
+        reader_login TEXT UNIQUE NOT NULL,
+        reader_password TEXT NOT NULL,
+
+        CONSTRAINT reader_pk PRIMARY KEY (reader_ID),
+        CONSTRAINT person_fk FOREIGN KEY (reader_ID) REFERENCES person (person_ID)     
+
+);
+
+CREATE TABLE commen (
+        comment_ID BIGINT,
+        comment_content_path TEXT NOT NULL,
+        comment_date DATE NOT NULL,
+        book_ISBN TEXT,
+        reader_ID BIGINT,    
+
+        CONSTRAINT commen_pk PRIMARY KEY (comment_ID),
+        CONSTRAINT book_fk FOREIGN KEY (book_ISBN) REFERENCES book (book_ISBN),
+        CONSTRAINT reader_fk FOREIGN KEY (reader_ID) REFERENCES reader (reader_ID)         
+
 );
--- insert_data.sql
+++ insert_data.sql
@@ -1,6 +1,45 @@
-﻿
-INSERT INTO theme (theme_name) VALUES ('Детектив');
+﻿INSERT INTO theme (theme_name) VALUES ('Детектив');
 INSERT INTO theme (theme_name) VALUES ('Драма');
+INSERT INTO theme (theme_name) VALUES ('Комедия');
+INSERT INTO theme (theme_name) VALUES ('Наука');
 
 INSERT INTO book (book_isbn, book_name, book_path, theme_name, publisher_id) VALUES ('abc', 'abc', NULL, 'Детектив', NULL);
 INSERT INTO book (book_isbn, book_name, book_path, theme_name, publisher_id) VALUES ('abc2', 'abc2', NULL, 'Драма', NULL);
+INSERT INTO book (book_isbn, book_name, book_path, theme_name, publisher_id) VALUES ('abc3', 'abc3', NULL, 'Комедия', NULL);
+INSERT INTO book (book_isbn, book_name, book_path, theme_name, publisher_id) VALUES ('abc4', 'abc4', NULL, 'Наука', NULL);
+
+INSERT INTO publisher (publisher_id, publisher_name, publusher_country, publisher_city, publisher_street, publisher_house) VALUES (1, 'Первое', 'Russia', 'Moscow', 'Tverskaya', '1');
+INSERT INTO publisher (publisher_id, publisher_name, publusher_country, publisher_city, publisher_street, publisher_house) VALUES (2, 'Второе', 'Germany', 'Berlin', 'Unter der Linden', '5');
+INSERT INTO publisher (publisher_id, publisher_name, publusher_country, publisher_city, publisher_street, publisher_house) VALUES (3, 'Третье', 'Russia', 'Samara', 'Lenina', '3');
+INSERT INTO publisher (publisher_id, publisher_name, publusher_country, publisher_city, publisher_street, publisher_house) VALUES (4, 'Четвертое', 'Russia', 'Samara', 'Kirova', '33');
+
+INSERT INTO reader_book (reader_book_book_ISBN, reader_book_reader_ID) VALUES ('abc', 3);
+INSERT INTO reader_book (reader_book_book_ISBN, reader_book_reader_ID) VALUES ('abc2', 3);
+INSERT INTO reader_book (reader_book_book_ISBN, reader_book_reader_ID) VALUES ('abc3', 5);
+INSERT INTO reader_book (reader_book_book_ISBN, reader_book_reader_ID) VALUES ('abc4', 2);
+
+INSERT INTO author_book (author_book_book_ISBN, author_book_reader_ID) VALUES ('abc', 1);
+INSERT INTO author_book (author_book_book_ISBN, author_book_reader_ID) VALUES ('abc2', 3);
+INSERT INTO author_book (author_book_book_ISBN, author_book_reader_ID) VALUES ('abc3', 4);
+INSERT INTO author_book (author_book_book_ISBN, author_book_reader_ID) VALUES ('abc4', 5);
+
+INSERT INTO person (person_ID, person_last_name, person_first_name, person_middle_name, person_birthdate) VALUES (1, 'Ivanov', 'Ivan', NULL, 2001-05-21); 
+INSERT INTO person (person_ID, person_last_name, person_first_name, person_middle_name, person_birthdate) VALUES (2, 'Petrov', 'Petr', 'Ivanovich', 1969-03-06);
+INSERT INTO person (person_ID, person_last_name, person_first_name, person_middle_name, person_birthdate) VALUES (3, 'Ivanova', 'Elena', NULL, 2000-11-29);
+INSERT INTO person (person_ID, person_last_name, person_first_name, person_middle_name, person_birthdate) VALUES (4, 'Naumov', 'Petr', 'Ivanovich', 1993-06-15);
+
+INSERT INTO author (author_ID) VALUES (1);
+INSERT INTO author (author_ID) VALUES (2);
+INSERT INTO author (author_ID) VALUES (3);
+INSERT INTO author (author_ID) VALUES (4);
+
+INSERT INTO reader (reader_ID, reader_login, reader_password) VALUES (1, '365', '3');
+INSERT INTO reader (reader_ID, reader_login, reader_password) VALUES (2, '36', '3');
+INSERT INTO reader (reader_ID, reader_login, reader_password) VALUES (3, '65', '8');
+INSERT INTO reader (reader_ID, reader_login, reader_password) VALUES (4, '3658', '9');
+
+INSERT INTO commen (commen_ID, commen_content_path, commen_date, book_ISBN, reader_ID) VALUES (1, 'path', 2014-03-21, 'abc', 3); 
+INSERT INTO commen (commen_ID, commen_content_path, commen_date, book_ISBN, reader_ID) VALUES (2, 'path2', 2016-12-15, 'abc', 2); 
+INSERT INTO commen (commen_ID, commen_content_path, commen_date, book_ISBN, reader_ID) VALUES (3, 'path3', 2013-07-13, 'abc3', 2); 
+INSERT INTO commen (commen_ID, commen_content_path, commen_date, book_ISBN, reader_ID) VALUES (4, 'path4', 2016-08-02, 'abc3', 4); 
+
